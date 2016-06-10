e862e5fe5c6403b4d03ba000ca9e23a3bcae8c06
Brett New <brettnew@Bretts-MacBook-Air.local>
2016-06-10 17:07:33 -0500

    display alert for all numbers and all specs being met.

--- ping-pong/js/scripts.js
+++ ping-pong/js/scripts.js
@@ -1,17 +1,3 @@
-//
-//
-// if (!numberInput) {
-//   alert ("Please enter a valid integer!");
-// }
-// else if {
-//   for (var i = 0; i < numberInput; i ++){
-//     var numberDisplay = i;
-//   }
-// }
-
-
-
-
 
 
 
@@ -22,124 +8,23 @@ $(document).ready(function(){
     event.preventDefault();
     var numberInput = parseInt($("input#number").val());
     // var numberList = $("ul").append("<li>" + numberInput + "</li>");
-debugger;
-
-var numbersArray = [];
-for(var index = 1; index <= numberInput; index++){
-  if (index%3 === 0){
-    numbersArray.push("ping")
-  }
-}
-//   numbersArray.push(index);
-// }
-alert(numbersArray);
-var numbersList = function (number){
-  var newArray = numbersArray.map(function(value){
-  if (value%15 === "0"){
-    return "pingpong"
-  }
-});
-}
-
-
-
-    // var result = function(numberInput){
-    //   for (var i = 1; i <= numberInput; i++){
-    //     if (isNaN(numberInput) || (numberInput%1 != 0)) {
-    //       alert("Please enter a valid integer!");
-    //     }
-    //
-    //   }
-    // }
-
-
-
-
-
-
-
-
-
-//
 // debugger;
-//
-//   var numbersArray = [];
-//   var numbersList = function(numberInput){
-//    for (var i=0; i <= numberInput; i++){
-//     if(i%15===0){
-//       numbersArray.push("pingpong");
-//     }
-//     else {
-//       numbersArray.push(i);
-//     }
-//   };
-//   return numbersArray;
-// };
-// alert(numbersArray);
 
-
-
-
-
-
-
-    // var result = numbersArray
-    // $("ul").append("<li>" + result  +"</li>")
-//   });
-// });
-
-
-// STring
-// debugger;
-// for (var i = 1; i <= numberInput; i++){
-//   var numberList = i
-//   // $("ul").append("<li>" + numberList +"</li>");
-//   var replaceNumber = function (numberInput){
-//     if (i%3===0) {
-//       return numberList.replace(i, "ping");
-//     }
-//   $("ul").append("<li>" + numberList +"</li>");
-// alert (numberList);
-//   }
-// }
-
-
-
-
-//
-// var numbersArray = [];
-// for (var i = 1; i <= numberInput; i++){
-//   numbersArray.push(i);
-// }
-
-// for (var i =0; i < numbersArray.length; i++){
-//   var indexNumbers = numbersArray[i];
-//   if (parseInt(indexNumbers)%3===0){
-//     indexNumbers = 'ping';
-//     alert(numbersArray);
-//   }
-// }
-
-
-
-
-
-// alert(numbersArray);
-// // var numberList = function(numberInput){
-// // var numbersArray = [];
-// // for (var i = 1; i <= numberInput; i++){
-// //
-// // }
-// //   // if (!numberInput) {
-//   //   alert ("Please enter a valid integer!");
-//   //   }
-// //     else if (numberInput ){}
-// //       for (var i = 1; i <= numberInput; i ++){
-// //         var numberDisplay = i;
-// //     }
-// //   }
-// // }
-//
-//
-});
+    var numbersArray = [];
+    for(var index = 1; index <= numberInput; index++){
+     if (index%15 === 0){
+        numbersArray.push("pingpong");
+      }
+      else if (index%5 === 0){
+        numbersArray.push("pong");
+      }
+      else if (index%3 === 0){
+        numbersArray.push("ping");
+      }
+      else {
+        numbersArray.push(index);
+      }
+    }
+alert (numbersArray);
+  });
 });
